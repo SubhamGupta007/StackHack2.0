95a0be04f0f8b3e4181133b2ec7d31f5b52c0683
SubhamGupta007 <56687896+SubhamGupta007@users.noreply.github.com>
2021-01-19 11:32:25 +0530

    update

--- StackHackBackend-main/client/urls.py
+++ StackHackBackend-main/client/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from client import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
     path('menu/', views.menu, name='menu')
 ]
